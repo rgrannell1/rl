06798f6b2321cbab0f894e5861a71a086243990f
Róisín Grannell <r.grannell2@gmail.com>
2021-08-22 01:04:09 +0100

    tidied source

--- rl.go
+++ rl.go
@@ -11,7 +11,7 @@ import (
 	"github.com/eiannone/keyboard"
 )
 
-// Open /dev/tty
+// Open /dev/tty with user write permissions.
 func OpenTTY() (*os.File, error) {
 	fd, err := syscall.Open("/dev/tty", syscall.O_WRONLY, 00200)
 
@@ -23,15 +23,18 @@ func OpenTTY() (*os.File, error) {
 }
 
 type LineChangeState struct {
+	lineBuffer *[]rune
+	done       bool
+	cmd        *exec.Cmd
+}
+
+type LineChangeCtx struct {
 	shell       string
 	tty         *os.File
-	linebuffer  *[]rune
 	show        bool
 	inputOnly   bool
 	execute     *string
-	done        bool
 	environment []string
-	cmd         *exec.Cmd
 }
 
 // Stop a running execute process
@@ -44,28 +47,34 @@ func StopProcess(state *LineChangeState) {
 	}
 }
 
+const CLEAR_STRING = "\033[H\033[2J"
+
 // Run each time a user inputs a character
-func OnUserInputChange(stateChan chan LineChangeState, cmdLock *sync.Mutex) {
+func OnUserInputChange(stateChan chan LineChangeState, cmdLock *sync.Mutex, ctx *LineChangeCtx) {
 	state := <-stateChan
 
 	cmdLock.Lock()
+	defer func() {
+		cmdLock.Unlock()
+	}()
+
 	StopProcess(&state)
 
-	if !state.show {
-		state.tty.Write([]byte("\033[H\033[2J"))
+	if !ctx.show {
+		ctx.tty.Write([]byte(CLEAR_STRING))
 	}
 
-	line := string(*state.linebuffer)
+	line := string(*state.lineBuffer)
 
-	if len(*state.execute) == 0 {
+	if len(*ctx.execute) == 0 {
 		// no command to execute
 		if state.done {
 			os.Stdout.WriteString(line + "\n")
 		} else {
-			state.tty.WriteString(line + "\n")
+			ctx.tty.WriteString(line + "\n")
 		}
 		return
-	} else if state.done && state.inputOnly {
+	} else if state.done && ctx.inputOnly {
 		// we're done, we only want the input line but not the command output
 		os.Stdout.WriteString(line + "\n")
 
@@ -74,27 +83,25 @@ func OnUserInputChange(stateChan chan LineChangeState, cmdLock *sync.Mutex) {
 	}
 
 	// run the provided command in the users shell
-	cmd := exec.Command(state.shell, "-c", *state.execute)
+	cmd := exec.Command(ctx.shell, "-c", *ctx.execute)
 
 	// by default, go will use the current  process's environment. Add RL_INPUT to the list.
-	cmd.Env = append(state.environment, "RL_INPUT="+line)
+	cmd.Env = append(ctx.environment, "RL_INPUT="+line)
 	cmd.Stderr = os.Stderr
 
 	// only output the last command to standard-output by default; otherwise just show it on the tty
 	if state.done {
 		cmd.Stdout = os.Stdout
 	} else {
-		cmd.Stdout = state.tty
+		cmd.Stdout = ctx.tty
 	}
 
 	state.cmd = cmd
 
-	// non-blocking command-start;
+	// non-blocking command-start
 	state.cmd.Start()
 
 	stateChan <- state
-
-	cmdLock.Unlock()
 }
 
 // Start the interactive line-editor
@@ -112,28 +119,23 @@ func rl(show bool, inputOnly bool, execute *string) {
 		keyboard.Close()
 	}()
 
-	linebuffer := []rune{}
-
 	tty, ttyErr := OpenTTY()
 
 	if ttyErr != nil {
-		fmt.Printf("RL: failed to open /dev/tty. %v\n", ttyErr)
+		fmt.Printf("RL: could not open /dev/tty. Are you running rl non-interactively?")
 		os.Exit(1)
 	}
 	defer func() {
 		tty.Close()
 	}()
 
-	state := LineChangeState{
+	ctx := LineChangeCtx{
 		os.Getenv("SHELL"),
 		tty,
-		&linebuffer,
 		show,
 		inputOnly,
 		execute,
-		false,
 		os.Environ(),
-		nil,
 	}
 
 	stateChan := make(chan LineChangeState)
@@ -146,6 +148,16 @@ func rl(show bool, inputOnly bool, execute *string) {
 		close(doneChan)
 	}()
 
+	cmdLock := &sync.Mutex{}
+
+	lineBuffer := []rune{}
+	state := LineChangeState{
+		&lineBuffer,
+		false,
+		nil,
+	}
+
+	var done bool
 	for {
 		// repeatedly get keys, until a terminating character is reached
 		char, key, err := keyboard.GetKey()
@@ -155,33 +167,35 @@ func rl(show bool, inputOnly bool, execute *string) {
 			os.Exit(1)
 		}
 
-		if key == keyboard.KeyBackspace || key == keyboard.KeyBackspace2 {
-			if len(linebuffer) > 0 {
-				// backspace should remove the last element in a buffer
-				linebuffer = linebuffer[:len(linebuffer)-1]
-			}
-		} else if key == keyboard.KeyCtrlC || key == keyboard.KeyEsc || key == keyboard.KeyEnter {
-			// exit interactive editor
+		lineBuffer, done = UpdateLineBuffer(char, key, lineBuffer)
 
-			state.done = true
-		} else if key == keyboard.KeySpace {
-			// handle spaces
-			linebuffer = append(linebuffer, ' ')
-		} else {
-			// -- append character to the buffer
-			linebuffer = append(linebuffer, char)
-		}
+		state.lineBuffer = &lineBuffer
+		state.done = done
 
-		cmdLock := &sync.Mutex{}
-
-		// handle command execution!! Factor this out when I can
-		go OnUserInputChange(stateChan, cmdLock)
-
+		go OnUserInputChange(stateChan, cmdLock, &ctx)
 		stateChan <- state
+
 		state = <-stateChan
 
 		if state.done {
 			break
 		}
 	}
+}
+
+// Update the user input line-buffer
+func UpdateLineBuffer(char rune, key keyboard.Key, lineBuffer []rune) ([]rune, bool) {
+	if key == keyboard.KeyBackspace || key == keyboard.KeyBackspace2 {
+		if len(lineBuffer) == 0 {
+			return []rune{}, false
+		} else {
+			return lineBuffer[:len(lineBuffer)-1], false
+		}
+	} else if key == keyboard.KeyCtrlC || key == keyboard.KeyEsc || key == keyboard.KeyEnter {
+		return lineBuffer, true
+	} else if key == keyboard.KeySpace {
+		return append(lineBuffer, ' '), false
+	}
+
+	return append(lineBuffer, char), false
 }
